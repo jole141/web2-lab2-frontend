cfd963467ab3c8a512e723abb5e4e8a79d176cd1
jjurenic <jj52362@fer.hr>
2022-11-07 22:23:20 +0100

    add proxy middleware fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,8 @@ const proxy = require("http-proxy-middleware").createProxyMiddleware;
 module.exports = function (app) {
   app.use(
     proxy(`/api/**`, {
-      target: process.env.REACT_APP_API_URL || "http://localhost:8000",
+      target:
+        process.env.REACT_APP_API_URL || "https://web2-lab2-api.onrender.com/",
     })
   );
 };
